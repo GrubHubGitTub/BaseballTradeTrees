2b40a79f799d7c86b08ff409c45f794f42ac4cc1
GrubHubGitTub <aidangruber@hotmail.com>
2022-10-11 23:38:07 +0700

    edit config

--- mlb-trade-trees-next/next.config.js
+++ mlb-trade-trees-next/next.config.js
@@ -1,23 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
+/** @type {import('next').NextConfig} */
 
-// module.exports = nextConfig
 
-module.exports = {
-  future: {
-    webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
-      // Looks like backward compatibility approach.
-  },
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-        // by next.js will be dropped. Doesn't make much sense, but how it is
-      fs: false, // the solution
-    };
-
-    return config;
-  },
-};
+module.exports = {reactStrictMode: true,
+swcMinify: true,
+future: { webpack5: true }
+}
