0bb7e30463e2e2704b5e2782ef10237faf666f66
GrubHubGitTub <aidangruber@hotmail.com>
2022-10-14 17:59:30 +0700

    fix relocate

--- mlb-trade-trees-next/components/Layout.js
+++ mlb-trade-trees-next/components/Layout.js
@@ -1,6 +1,6 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
-import player_search from "../data/player_search.json"
+import player_search from "../public/data/player_search.json"
 
 export default function Layout({ children }) {
   const players = player_search
