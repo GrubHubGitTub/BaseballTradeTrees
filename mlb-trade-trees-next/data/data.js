a6937c9c59ac7cc3c7fc145410a5841cf2c2395e
GrubHubGitTub <aidangruber@hotmail.com>
2022-10-14 21:00:54 +0700

    remove 1 json

--- mlb-trade-trees-next/data/data.js
+++ mlb-trade-trees-next/data/data.js
@@ -1,7 +1,7 @@
 import player_data1 from "../../data/tests-edits/all_data1.json"
-import player_data2 from "../../data/tests-edits/all_data2.json"
 
 export function getData() {
-    const data = player_data1.concat(player_data2);
+    // const data = player_data1.concat(player_data2);
+    const data = player_data1
     return data
 }
